2ff291a32fd00e8155169ab562afcea08b5e6bcc
ebartnik97 <128297075+ebartnik97@users.noreply.github.com>
2023-06-28 19:20:23 +0200

    Add files via upload

---
+++ Drawing/BattleCity/GameReferences.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleCity.Model;
+namespace BattleCity.references
+{
+    public static class GameReferences
+    {
+        public static GameField FieldRef { get; set; }
+        public static Stopwatch Stopwatch { get; set; }
+    }
+}
